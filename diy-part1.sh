0cf7be6c5e0bd61481dbd0e5badb12653280506a
liaoweiliao521 <liaoweiliao521@gmail.com>
2021-08-12 22:27:39 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,10 +15,4 @@
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-
-# 取消helloworld源注释（ssr-plus）
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
-# 添加xiaorouji大passwall源
-sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
