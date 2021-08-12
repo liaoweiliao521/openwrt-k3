cd750aa179a706d41742176350577aa9804a35bb
liaoweiliao521 <liaoweiliao521@gmail.com>
2021-08-13 01:22:45 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,11 +16,11 @@ sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generat
 # 取消Lean大登陆密码
 sed -i 's/^\(.*99999\)/#&/' package/lean/default-settings/files/zzz-default-settings
 
-# 删除原版trojan插件
-#rm -rf package/lean/trojan
-
 # 拉取sensecluci-app-udp2raw插件
 git clone https://github.com/sensec/luci-app-udp2raw.git package/luci-app-udp2raw
 
 # 拉取sensecluci-app-udp2raw-control插件
 git clone https://github.com/sensec/openwrt-udp2raw.git package/openwrt-udp2raw
+
+# 拉取openwrt-passwall插件
+git clone https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
