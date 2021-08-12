273b59511fd837fb3b9c6ad5a570aa347e62d2d9
liaoweiliao521 <liaoweiliao521@gmail.com>
2021-08-13 01:26:05 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,10 +11,10 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
 
 # 取消Lean大登陆密码
-sed -i 's/^\(.*99999\)/#&/' package/lean/default-settings/files/zzz-default-settings
+#sed -i 's/^\(.*99999\)/#&/' package/lean/default-settings/files/zzz-default-settings
 
 # 拉取sensecluci-app-udp2raw插件
 git clone https://github.com/sensec/luci-app-udp2raw.git package/luci-app-udp2raw
